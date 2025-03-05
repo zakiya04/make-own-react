fc5e00c8a32172e0de9d700f49d944658b98d30b
Zakiya <zakiyamanzoor04@gmail.com>
2025-03-05 19:40:36 +0530

    Update the elements based on reconcilliation

--- index.js
+++ index.js
@@ -50,8 +50,41 @@ function createDom(fiber) {
   return dom;
 }
 
+//we compare the old fiber with the new one and change accordingly//
+
+const isEvent = key => key.startsWith('on')
+const isProperty = key => key !== 'children' && !isEvent(key);
+const isNew = (prev, next) => key => prev[key] !== next[key];
+const isGone = (prev,next) => key => !(key in next);
+
 function updateDom(dom, prevProps, nextProps){
-  
+
+  //change eventlisteners or remove them//
+  Object.keys(prevProps).filter(isEvent)
+  .filter(key => !(key in nextProps) || isNew(prevProps,nextProps)(key))
+  .forEach(name =>{
+    const eventType = name.toLowerCase().substring(2);
+    dom.removeEventListener(eventType,prevProps[name])
+  })
+
+  // Remove old properties
+  Object.keys(prevProps)
+  .filter(isProperty)
+  .filter(isGone(prevProps, nextProps))
+  .forEach(name => {
+    dom[name] = ""
+  })
+
+  //add new property to elements//
+  Object.keys(nextProps).filter(isProperty).filter(isNew(prevProps,nextProps)).forEach(name =>{
+    dom[name] = nextProps[name]
+  })
+
+  //at last add the new event listeners//
+  Object.props(nextProps).filter(isEvent).filter(isNew(prevProps,nextProps).forEach(name =>{
+    const eventType = name.toLowerCase().substring(2);
+    dom.addEventListener(eventType,nextProps[name])
+  }))
 }
 
 //this is so as to recursively call the fiber to append to dom//
